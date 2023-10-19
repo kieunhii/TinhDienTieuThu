41756086f7220a2b396609aeb61fdb278aba4804
kieunhii <truongnhi674200@gmmail.com>
2023-10-19 21:21:14 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿
+namespace TinhDienTieuThu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtTieuThu = new System.Windows.Forms.TextBox();
+            this.txtThanhTien = new System.Windows.Forms.TextBox();
+            this.txtSM = new System.Windows.Forms.TextBox();
+            this.txtDM = new System.Windows.Forms.TextBox();
+            this.txtSC = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.cboKV = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnTinhTien = new System.Windows.Forms.Button();
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtTong = new System.Windows.Forms.TextBox();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(132, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Báo Cáo Tiêu Thụ Điện";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ Tên KH";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Khu Vực";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Định Mức ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Chỉ Số Cũ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Chỉ Số Mới";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 353);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Thành Tiền";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 308);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Tiêu Thụ ";
+            // 
+            // txtTieuThu
+            // 
+            this.txtTieuThu.Location = new System.Drawing.Point(116, 305);
+            this.txtTieuThu.Name = "txtTieuThu";
+            this.txtTieuThu.ReadOnly = true;
+            this.txtTieuThu.Size = new System.Drawing.Size(177, 22);
+            this.txtTieuThu.TabIndex = 8;
+            // 
+            // txtThanhTien
+            // 
+            this.txtThanhTien.Location = new System.Drawing.Point(116, 350);
+            this.txtThanhTien.Name = "txtThanhTien";
+            this.txtThanhTien.ReadOnly = true;
+            this.txtThanhTien.Size = new System.Drawing.Size(177, 22);
+            this.txtThanhTien.TabIndex = 9;
+            // 
+            // txtSM
+            // 
+            this.txtSM.Location = new System.Drawing.Point(116, 264);
+            this.txtSM.Name = "txtSM";
+            this.txtSM.Size = new System.Drawing.Size(177, 22);
+            this.txtSM.TabIndex = 10;
+            // 
+            // txtDM
+            // 
+            this.txtDM.Location = new System.Drawing.Point(116, 172);
+            this.txtDM.Name = "txtDM";
+            this.txtDM.ReadOnly = true;
+            this.txtDM.Size = new System.Drawing.Size(177, 22);
+            this.txtDM.TabIndex = 11;
+            // 
+            // txtSC
+            // 
+            this.txtSC.Location = new System.Drawing.Point(116, 224);
+            this.txtSC.Name = "txtSC";
+            this.txtSC.Size = new System.Drawing.Size(177, 22);
+            this.txtSC.TabIndex = 12;
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(116, 68);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(177, 22);
+            this.txtTen.TabIndex = 13;
+            // 
+            // cboKV
+            // 
+            this.cboKV.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboKV.FormattingEnabled = true;
+            this.cboKV.Items.AddRange(new object[] {
+            "Khu Vực 1",
+            "Khu Vực 2",
+            "Khu Vực 3"});
+            this.cboKV.Location = new System.Drawing.Point(116, 114);
+            this.cboKV.Name = "cboKV";
+            this.cboKV.Size = new System.Drawing.Size(177, 24);
+            this.cboKV.TabIndex = 14;
+            this.cboKV.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnTinhTien
+            // 
+            this.btnTinhTien.Location = new System.Drawing.Point(0, 394);
+            this.btnTinhTien.Name = "btnTinhTien";
+            this.btnTinhTien.Size = new System.Drawing.Size(94, 23);
+            this.btnTinhTien.TabIndex = 15;
+            this.btnTinhTien.Text = "Tính Tiền";
+            this.btnTinhTien.UseVisualStyleBackColor = true;
+            this.btnTinhTien.Click += new System.EventHandler(this.btnTinhTien_Click);
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Location = new System.Drawing.Point(100, 394);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(94, 23);
+            this.btnNhap.TabIndex = 16;
+            this.btnNhap.Text = "Nhập Mới ";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(200, 394);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(93, 23);
+            this.btnThoat.TabIndex = 17;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(328, 18);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(587, 354);
+            this.listView1.TabIndex = 18;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Họ Tên";
+            this.columnHeader1.Width = 81;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Khu Vực";
+            this.columnHeader2.Width = 88;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Định Mức";
+            this.columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Tiêu Thụ";
+            this.columnHeader4.Width = 88;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Thành Tiền";
+            this.columnHeader5.Width = 139;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(325, 397);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Tổng Tiền ";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // txtTong
+            // 
+            this.txtTong.Location = new System.Drawing.Point(459, 392);
+            this.txtTong.Name = "txtTong";
+            this.txtTong.ReadOnly = true;
+            this.txtTong.Size = new System.Drawing.Size(164, 22);
+            this.txtTong.TabIndex = 20;
+            this.txtTong.Text = "0";
+            this.txtTong.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(799, 395);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(83, 22);
+            this.btnXoa.TabIndex = 21;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 450);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.txtTong);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnNhap);
+            this.Controls.Add(this.btnTinhTien);
+            this.Controls.Add(this.cboKV);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtSC);
+            this.Controls.Add(this.txtDM);
+            this.Controls.Add(this.txtSM);
+            this.Controls.Add(this.txtThanhTien);
+            this.Controls.Add(this.txtTieuThu);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtTieuThu;
+        private System.Windows.Forms.TextBox txtThanhTien;
+        private System.Windows.Forms.TextBox txtSM;
+        private System.Windows.Forms.TextBox txtDM;
+        private System.Windows.Forms.TextBox txtSC;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.ComboBox cboKV;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnTinhTien;
+        private System.Windows.Forms.Button btnNhap;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtTong;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TinhDienTieuThu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboKV.Text == "Khu Vực 1")
+            {
+                txtDM.Text = "50";
+            }
+            if (cboKV.Text == "Khu Vực 2")
+            {
+                txtDM.Text = "100";
+            }
+            if (cboKV.Text == "Khu Vực 3")
+            {
+                txtDM.Text = "150";
+            }
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            if (Convert.ToInt32(txtSC.Text)>Convert.ToInt32(txtSM.Text))
+            {
+                errorProvider1.SetError(txtSM,"Vui long nhap lai so");
+            }
+            else
+            {
+                txtTieuThu.Text = (Convert.ToInt32(txtSM.Text)-Convert.ToInt32(txtSC.Text)).ToString();
+                if (Convert.ToInt32(txtTieuThu.Text)<Convert.ToInt32(txtDM.Text))
+                {
+                    txtThanhTien.Text = (Convert.ToInt32(txtTieuThu.Text)*500).ToString();
+                }
+                else
+                {
+                    txtThanhTien.Text = (Convert.ToInt32(txtTieuThu.Text) * 1000).ToString();
+                }
+
+                ListViewItem item = new ListViewItem();
+                item.Text = txtTen.Text;
+                listView1.Items.Add(item);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = cboKV.Text
+                });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = txtDM.Text
+                }); item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = txtTieuThu.Text
+                }); item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = txtThanhTien.Text
+                });
+
+                txtTong.Text = (Convert.ToInt32(txtThanhTien.Text) + Convert.ToInt32(txtTong.Text)).ToString();
+            
+                
+            }
+
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            txtTen.Clear();
+            txtDM.Clear();
+            txtSC.Clear();
+            txtSM.Clear();
+            txtThanhTien.Clear();
+            txtTieuThu.Clear();
+            cboKV.ResetText();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                int viTri = listView1.SelectedIndices[0];
+                txtTong.Text = (Convert.ToInt32(txtTong.Text) - Convert.ToInt32(listView1.Items[viTri].SubItems[4].Text)).ToString();
+                listView1.Items.RemoveAt(viTri);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Can chon dong can xoa");
+            }
+
+
+        }
+       
+    }
+}
